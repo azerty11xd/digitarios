d4db4082481e20a4cb55303cbcba7cc1308b3453
malek <maleksaadani90@gmail.com>
2023-08-07 17:10:22 +0100

    modification

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <Header style={{ zIndex: 1000 }} />
+        <Header />
         {children}
       </body>
     </html>
